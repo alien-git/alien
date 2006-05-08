238d37d082e8be52d169fbd8d8acf7a4308b73b1
jgrosseo <jgrosseo@4525493e-7705-40b1-a816-d608a930855b>
2006-05-08 19:15:53 +0000

    added error message in case of general missing startup.conf

--- common/trunk/scripts/ServiceStatus.sh
+++ common/trunk/scripts/ServiceStatus.sh
@@ -4,33 +4,40 @@
 #
 
 ETCDIR=$HOME/.alien/etc/aliend
-. $ETCDIR/startup.conf
 
-ALL_ORG=$ALIEN_ORGANISATIONS
-
-if [ -n "$1" ] ;
+if [ -f $ETCDIR/startup.conf ]
 then
-   ALL_ORG="$*"
+  . $ETCDIR/startup.conf
+
+  ALL_ORG=$ALIEN_ORGANISATIONS
+
+  if [ -n "$1" ] ;
+  then
+     ALL_ORG="$*"
+  fi
+
+  ERRCODE=0
+
+  for ALIEN_ORGANISATION in $ALL_ORG
+  do
+      if [ -f $ETCDIR/$ALIEN_ORGANISATION/startup.conf ];
+      then
+        . $ETCDIR/$ALIEN_ORGANISATION/startup.conf
+        for service in $AliEnServices
+        do
+          $AliEnCommand --org $ALIEN_ORGANISATION Status$service -silent > /dev/null 2>&1
+          error=$?
+          echo -e "$service\tStatus\t$error"
+        done
+      else
+        ERRMSG="File $ETCDIR/$ALIEN_ORGANISATION/startup.conf does not exist!"
+        ERRCODE=2
+      fi
+  done
+else
+  ERRMSG="File $ETCDIR/startup.conf does not exist!"
+  ERRCODE=1
 fi
-
-ERRCODE=0
-
-for ALIEN_ORGANISATION in $ALL_ORG
-do
-    if [ -f $ETCDIR/$ALIEN_ORGANISATION/startup.conf ];
-    then
-      . $ETCDIR/$ALIEN_ORGANISATION/startup.conf
-      for service in $AliEnServices
-      do
-        $AliEnCommand --org $ALIEN_ORGANISATION Status$service -silent > /dev/null 2>&1
-        error=$?
-        echo -e "$service\tStatus\t$error"
-      done
-    else
-      ERRMSG="File $ETCDIR/$ALIEN_ORGANISATION/startup.conf does not exist!"
-      ERRCODE=1
-    fi
-done
 
 echo
 echo -e "SCRIPTRESULT\tStatus\t$ERRCODE\tMessage\t$ERRMSG"
