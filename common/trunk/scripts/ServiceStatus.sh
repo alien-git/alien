510f4d67c785eb80779fc1c83b64dc1137ff25fb
catalin <catalin@4525493e-7705-40b1-a816-d608a930855b>
2006-06-09 18:23:16 +0000

    if startup.conf files are not defined, pick the default VO and services

--- common/trunk/scripts/ServiceStatus.sh
+++ common/trunk/scripts/ServiceStatus.sh
@@ -3,43 +3,51 @@
 # ServiceStatus.sh Checks all services, output parsed by MonaLisa
 #
 
+ERRCODE=0
+ERRMSG=""
 ETCDIR=$HOME/.alien/etc/aliend
 
-if [ -f $ETCDIR/startup.conf ]
-then
-  . $ETCDIR/startup.conf
-
-  ALL_ORG=$ALIEN_ORGANISATIONS
-
-  if [ -n "$1" ] ;
-  then
-     ALL_ORG="$*"
-  fi
-
-  ERRCODE=0
-
-  for ALIEN_ORGANISATION in $ALL_ORG
-  do
-      if [ -f $ETCDIR/$ALIEN_ORGANISATION/startup.conf ];
-      then
-        . $ETCDIR/$ALIEN_ORGANISATION/startup.conf
-        for service in $AliEnServices
-        do
-          $AliEnCommand --org $ALIEN_ORGANISATION Status$service -silent > /dev/null 2>&1
-          error=$?
-          echo -e "$service\tStatus\t$error"
-        done
-      else
-        ERRMSG="File $ETCDIR/$ALIEN_ORGANISATION/startup.conf does not exist!"
-        ERRCODE=2
-      fi
-  done
+if [ -r $ETCDIR/startup.conf ] ; then
+	. $ETCDIR/startup.conf
+	ALL_ORG=$ALIEN_ORGANISATIONS
 else
-  ERRMSG="File $ETCDIR/startup.conf does not exist!"
-  ERRCODE=1
+	ALL_ORG="ALICE"
+	ERRCODE=2
+	ERRMSG="Global $ETCDIR/startup.conf not found, assuming ALICE."
 fi
 
-echo
-echo -e "SCRIPTRESULT\tStatus\t$ERRCODE\tMessage\t$ERRMSG"
+if [ -n "$1" ] ; then
+	ALL_ORG="$*"
+	ERRCODE=0
+	ERRMSG="Using parameters: $*"
+fi
 
+for ALIEN_ORGANISATION in $ALL_ORG ; do
+	if [ -r $ETCDIR/$ALIEN_ORGANISATION/startup.conf ] ; then
+		. $ETCDIR/$ALIEN_ORGANISATION/startup.conf
+	else
+		AliEnServices="Monitor CE SE PackMan MonaLisa"
+		AliEnCommand="$ALIEN_ROOT/bin/alien"
+		ERRCODE=2
+		ERRMSG="$ERRMSG Cannot read $ETCDIR/$ALIEN_ORGANISATION/startup.conf, assuming default services."
+	fi
+	for service in $AliEnServices ; do
+		$AliEnCommand --org $ALIEN_ORGANISATION Status$service -silent > /dev/null 2>&1
+		error=$?
+		if [ "$error" != "0" ] ; then
+			if [ ! -x $AliEnCommand ] ; then
+				echo -e "$service\tStatus\t$error\tMessage\tCannot execute $AliEnCommand"
+			else
+				echo -e "$service\tStatus\t$error\tMessage\tExit code $error"
+			fi
+		else
+			echo -e "$service\tStatus\t$error"
+		fi
+	done
+done
+if [ -n "$ERRMSG" ] ; then
+	echo -e "SCRIPTRESULT\tStatus\t$ERRCODE\tMessage\t$ERRMSG"
+else
+	echo -e "SCRIPTRESULT\tStatus\t$ERRCODE"
+fi
 
