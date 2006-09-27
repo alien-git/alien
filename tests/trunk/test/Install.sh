1aabfd74110bbdee4def9772ce7f538cfd9df86b
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2006-09-27 16:17:37 +0000

    Disabling the banking tests

--- tests/trunk/test/Install.sh
+++ tests/trunk/test/Install.sh
@@ -433,7 +433,7 @@ EXECUTE_SHELL()
     endTime=`date +"%s"`
     SEND_TO_ML "${group}_nTests" $nCmds "${group}_nSuccess" $nCmds "${group}_pSuccess" 100 "${group}_time" `expr $endTime - $startTime`
 }
-BANK_TESTS_LIST="304-putBankDataLDAP 301-bankAccount 302-transactFunds 303-addFunds "
+#BANK_TESTS_LIST="304-putBankDataLDAP 301-bankAccount 302-transactFunds 303-addFunds "
 
 JOB_TESTS_LIST="$BANK_TESTS_LIST 70-x509 89-jdl 19-ClusterMonitor 21-submit 73-updateCE 22-execute 305-execOrder 62-inputfile 23-resubmit 26-ProcessMonitorOutput 305-execOrder 105-killRunningJob 85-inputdata 94-inputpfn 98-jobexit 64-jobemail 77-rekill 86-split 87-splitFile 88-splitArguments 115-queueList 118-validateJob 119-outputDir 120-production 124-OutputArchive 126-OutputInSeveralSE 133-queueInfo 134-dumplist 135-inputdata2 137-userArchive 140-jobWithMemory 141-executingTwoJobs 152-inputdatacollection 153-splitInputDataCollection 157-zip 159-bigoutput 160-JDLenvironment 161-userGUID"
 PACKAGE_TESTS_LIST="75-PackMan 76-jobWithPackage 82-packageDependencies 84-sharedPackage 100-tcshPackage 83-gccPackage 130-localConfig 131-definedPackage"
@@ -447,7 +447,7 @@ GAPI_TESTS_LIST="500-apiservice 501-apiservice-connect 502-apiservice-motd 600-a
 CVS_INSTALL_LIST="10-cvs-co "
 
 
-NEW_VO_LIST="01-use 02-classads 04-createorgldap 03-createorgdb 61-rotate 05-createorgservices 300-prepareBankService 49-uninitialized"
+NEW_VO_LIST="01-use 02-classads 04-createorgldap 03-createorgdb 61-rotate 05-createorgservices 49-uninitialized"
 PERFORMANCE_TESTS_LIST="106-performanceInsert 108-performanceQuery 107-performanceDelete"
 
 
