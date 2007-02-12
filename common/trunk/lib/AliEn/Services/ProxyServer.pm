e216c4c69bbb46b33176929f7e525b100c018d3d
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2007-02-12 15:11:36 +0000

    Putting the name of the vo

--- common/trunk/lib/AliEn/Services/ProxyServer.pm
+++ common/trunk/lib/AliEn/Services/ProxyServer.pm
@@ -360,7 +360,7 @@ sub main {
       $server->{LOGGER}->critical( "ProxyServer", "Cannot create socket: $!" );
       $server->{LOGGER}->info( "ProxyServer",
       "Starting the ProxyServer in $server->{'localport'}" );
-    system("env >$ENV{HOME}/.alien/var/log/AliEn/pcegee02/ProxyServer.env");
+    system("env >$ENV{HOME}/.alien/var/log/AliEn/$self->{LOGGER}->{CONFIG}->{ORG_NAME}/ProxyServer.env");
      $server->Bind();
 }
 
