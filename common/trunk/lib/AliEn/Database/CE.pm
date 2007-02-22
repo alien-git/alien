09a8083f16516c053ff19cb47aea9d88a658a3f1
stefano <stefano@4525493e-7705-40b1-a816-d608a930855b>
2007-02-22 15:54:13 +0000

    Quotes added in query (fixes the "Attempt to fetch row from a Non-SLECT statement" warning)

--- common/trunk/lib/AliEn/Database/CE.pm
+++ common/trunk/lib/AliEn/Database/CE.pm
@@ -87,7 +87,7 @@ sub removeJobAgent {
        $batchId = $data->{batchId};
      } else {
        $self->debug(1,"Will try to cleanup JA with $key=$data->{$key}");
-       my $result = $self->query("SELECT batchId FROM JOBAGENT WHERE $key=$data->{$key}");
+       my $result = $self->query("SELECT batchId FROM JOBAGENT WHERE $key=\'$data->{$key}\'");
        $result = (@$result)[0]; # take the first and hopefully only one
        $result->{batchId} and $batchId = $result->{batchId};
        $self->debug(1,"batchId is $batchId");
