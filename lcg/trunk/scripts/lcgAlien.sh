49f79145c4eed6cca0f53d1a6309a5a50e6b2292
stefano <stefano@4525493e-7705-40b1-a816-d608a930855b>
2007-03-09 12:10:56 +0000

    Handle multiple DNs for one user (try all until success)

--- lcg/trunk/scripts/lcgAlien.sh
+++ lcg/trunk/scripts/lcgAlien.sh
@@ -4,12 +4,18 @@ AliEnCommand=$VO_ALICE_SW_DIR/alien/bin/alien
 
 uid=`$AliEnCommand --printenv | grep ALIEN_USER | cut --d='='  -f2`
 host=`$AliEnCommand -user aliprod --exec echo LDAPHOST | cut -d\' -f2`
-dn=`ldapsearch -x -LLL -H ldap://$host -b uid=$uid,ou=people,o=alice,dc=cern,dc=ch subject| perl -p -00 -e 's/\n //; s/subject: //; s/^dn:.*\n//'`
-proxy=`vobox-proxy --vo alice -dn "$dn" query-proxy-filename`
-error=$?
-if [ $error -ne 0 ] || [ "$proxy" == '' ] ;
-then
-   echo "Error setting proxy" 1>&2
-   exit 3
-fi
-env X509_USER_PROXY=$proxy $AliEnCommand $*
+dns=`ldapsearch -x -LLL -H ldap://$host -b uid=$uid,ou=people,o=alice,dc=cern,dc=ch subject| perl -p -00 -e 's/\n\n/\n/g;s/^dn:.*\n//g;s/\n //g;s/subject: //g;s/ /\#\#\#/g'`
+for line in $dns
+do
+  dn=`echo $line|sed -e 's/###/ /g'`
+  echo "Trying $dn";
+  proxy=`vobox-proxy --vo alice -dn "$dn" query-proxy-filename`
+  error=$?
+  if [ $error -eq 0 ] 
+  then
+    env X509_USER_PROXY=$proxy $AliEnCommand $*
+    exit 0
+  fi
+done
+echo "Error setting proxy" 1>&2
+exit 3
