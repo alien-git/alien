ee8ccfe70877a46cd7be2319fa22ad00cd071747
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2008-02-15 14:06:15 +0000

    Checking the transfers that stay in ASSIGNED

---
+++ common/trunk/lib/AliEn/Service/Optimizer/Transfer/Assigned.pm
@@ -0,0 +1,50 @@
+package AliEn::Service::Optimizer::Transfer::Assigned;
+
+use strict;
+
+use vars qw (@ISA);
+use AliEn::Service::Optimizer::Transfer;
+
+push (@ISA, "AliEn::Service::Optimizer::Transfer");
+
+
+
+sub checkWakesUp {
+  my $self=shift;
+  my $silent=(shift or 0);
+  my $method="info";
+  my @silentData=();
+  $silent and $method="debug" and push @silentData, 1;
+
+  $self->$method(@silentData,"In checkTransferRequirements checking if we can archive some of the old transfers");
+
+  #Updating the transfers with status 'WAITING' and only one PFN
+  my $transfers=$self->{DB}->query("SELECT transferid,jdl FROM TRANSFERS where status='ASSIGNED' and  ctime<SUBTIME(now(), SEC_TO_TIME(1800))");
+
+  defined $transfers
+    or $self->{LOGGER}->warning( "TransferOptimizer", "In checkTransferRequirements error during execution of database query" )
+      and return;
+
+
+  $self->info( "There are ".($#{$transfers} +1)." transfers have been stuck in ASSIGNED for more than 30 minutes ");
+
+
+  foreach my $transfer (@$transfers){
+    $self->info("What can we do with '$transfer->{transferid}'?");
+    my $ca = Classad::Classad->new($transfer->{jdl});
+    $self->info( "Classad created");
+    my ($ok, $action)=$ca->evaluateAttributeString("Action");
+    $ok or $self->info("Error getting the action from the jdl!") and next;
+    my $status;
+    ($action =~ /transfer/) and  $status="LOCAL COPY";
+    ($action=~ /local copy/) and $status="WAITING";
+    ($action=~ /cleaning/) and $status="CLEANING";
+    
+    $status or $self->info("Don't know what to do about $action") and next;
+    $self->info("Setting the transfer to $status");
+    $self->{DB}->updateTransfer($transfer->{transferid}, {status=> "$status", jdl=>$transfer->{jdl}});
+  }
+
+
+  return ;
+}
