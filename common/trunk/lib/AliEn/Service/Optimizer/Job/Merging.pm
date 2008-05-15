2202d1f373c7cbff1afbdfd830bf96570d9c8d17
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2008-05-15 15:40:01 +0000

    Merging the jobs in FORCEMERGE

--- common/trunk/lib/AliEn/Service/Optimizer/Job/Merging.pm
+++ common/trunk/lib/AliEn/Service/Optimizer/Job/Merging.pm
@@ -28,7 +28,7 @@ sub checkWakesUp {
 
   $self->info("There are some jobs to check!!");
 
-  my $jobs=$self->{DB}->query("SELECT queueid, jdl, status from QUEUE q, JOBSTOMERGE j where q.queueid=j.masterid and status='SPLIT' ");
+    my $jobs=$self->{DB}->query("SELECT queueid, jdl, status from QUEUE q, JOBSTOMERGE j where q.queueid=j.masterid and status='SPLIT' union select queueid,jdl,status from QUEUE where status='FORCEMERGE'");
   foreach my $job (@$jobs){
     $self->{DB}->delete("JOBSTOMERGE", "masterId=?", {bind_values=>[$job->{queueid}]});
     use Data::Dumper;
