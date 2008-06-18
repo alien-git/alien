b30c46747e7a9433231b4bff153f44fb98dd4499
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2008-06-18 12:53:46 +0000

    Archiving the QUEUEPROC table

--- common/trunk/lib/AliEn/Service/Optimizer/Job/Expired.pm
+++ common/trunk/lib/AliEn/Service/Optimizer/Job/Expired.pm
@@ -14,158 +14,69 @@ sub checkWakesUp {
   my $method="info";
   $silent and $method="debug";
 
-
+  $self->{SLEEP_PERIOD}=3600;
   $self->{LOGGER}->$method("Expired", "In checkWakesUp .... optimizing the QUEUE table ...");
   my  $now = time; 
-  my $allkilled = $self->{DB}->getFieldsFromQueueEx("count(*) as count","where ( sent < (? - 7*86540) ) and ( status='KILLED' )", {bind_values=>[$now]});
-  my $allinqueue = $self->{DB}->getFieldsFromQueueEx("count(*) as count","");
 
-  my $olderthanoneyear = $self->{DB}->getFieldsFromQueueEx("count(*) as count","where ( sent < (? - 365*86540) ) ORDER by queueId", {bind_values=>[$now]});
 
-  if ( (! defined $allkilled)  || (! defined $allinqueue ) ) {
-    $self->info( "I cannot retrieve the number of entries in the queue .... Aborting check!");
-    return ;
-  }
+  $self->archiveJobs("received < (? - 365*86540)", "1 year","QUEUEEXPIRED" );
 
-  my $ratio;
-  $ratio =0 ;
+  $self->archiveJobs("( ( (status='DONE') || (status='FAILED') || (status='EXPIRED') || (status like 'ERROR%')  ) && ( received < (? - 28*86540) ) )", "4 weeks" ,$self->{DB}->{QUEUEARCHIVE});
 
-  if ( (defined @$allinqueue[0]->{count} )  && (@$allinqueue[0]->{count} != 0)) {
-    $ratio = 100.0 * int (@$allkilled[0]->{count}) / int (@$allinqueue[0]->{count});
-  }
-
-  $self->{LOGGER}->$method("Expired", "Found @$allkilled[0]->{count} killed Processes out of @$allinqueue[0]->{count} [Ratio $ratio]");
-
-  $self->{LOGGER}->$method("Expired", "Found @$olderthanoneyear[0]->{count} Processes out of @$allinqueue[0]->{count} older than 1 year");
-
-  if ( ($ratio < 0) || ($ratio > 80)) {
-    $self->{LOGGER}->$method("Expired", "I better do nothing, the ratio [$ratio] is out of limit .... Aborting check!");
-    return;
-  }
-
-  $self->{LOGGER}->$method("Expired", "Getting 10000 old killed jobs to expire");
-
-  my $data = $self->{DB}->getFieldsFromQueueEx("*","where ( sent < (? - 7*86540) ) and ( status='KILLED' ) ORDER by queueId limit 10000 ", {bind_values=>[$now]});
-
-  my $cnt = 0 ;
-  foreach (@$data) {
-      $cnt++;
-      $self->{LOGGER}->$method("Expired","[$cnt] Found Job $_->{queueId} >7 days old in status $_->{status}");
-      my $value;
-      # disabled for the moment	
-      $self->{DB}->insertEntry("QUEUEEXPIRED",$_) or 
-	  print STDERR "Expired: cannot copy entry $_->{queueId} to QUEUEEXPIRED\n"
-	      and return;
-      $self->{DB}->deleteJob("$_->{queueId}") or
-	  print STDERR "Expired: cannot delete entry $_->{queueId} from QUEUE\n"
-	      and return;
-  }
-
-  $self->{LOGGER}->$method("Expired", "Getting 10000 jobs older than 1 year to expire");
-
-  undef $data;
-
-  $data = $self->{DB}->getFieldsFromQueueEx("*","where ( sent < (? - 365*86540) ) ORDER by queueId", {bind_values=>[$now]});
-  $cnt=0;
-  foreach (@$data) {
-      $cnt++;
-      $self->info("[$cnt] Found Job $_->{queueId} > 1 year old in status $_->{status}");
-      my $value;
-      # disabled for the moment	
-      $self->{DB}->insertEntry("QUEUEEXPIRED",$_) or 
-	  print STDERR "Expired: cannot copy entry $_->{queueId} to QUEUEEXPIRED\n"
-	      and return;
-      $self->{DB}->deleteJob("$_->{queueId}") or
-	  print STDERR "Expired: cannot delete entry $_->{queueId} from QUEUE\n"
-	      and return;
-  }
-
-  $self->{LOGGER}->$method("Expired", "Archiving finished split jobs older than 1 week");
-
-  undef $data;
-
-  $data = $self->{DB}->getFieldsFromQueueEx("*","where ( (status='DONE' ) && ( jdl like '%split%') && ( received < (? - 7*86540) ) ) ORDER by queueId", {bind_values=>[$now]});
-  $cnt=0;
-  foreach (@$data) {
-      $cnt++;
-      $self->info("[$cnt] Found split job $_->{queueId} > 7 days finished  in status $_->{status}");
-      my $childjobs = $self->{DB}->getFieldsFromQueueEx("*","where (split=?) ORDER by queueId", {bind_values=>[$_->{queueId}]});
-      my $child;
-      $self->info("[$cnt] Moving master job $_->{queueId} to archive $self->{DB}->{QUEUEARCHIVE} ..");
-
-      foreach $child (@$childjobs) {
-	  $self->info("[$cnt] Moving childjob $child->{queueId} of master job $_->{queueId} to archive ..");
-	  # insert child jobs into the archive
-	  $self->{DB}->insertEntry("$self->{DB}->{QUEUEARCHIVE}",$child) or
-	      print STDERR "Expired: cannot copy entry $child->{queueId} to $self->{DB}->{QUEUEARCHIVE}\n"
-		  and next;
-	  
-	  # delete child job from the regular queue
-	  $self->{DB}->deleteJob("$child->{queueId}") or
-	      print STDERR "Expired: cannot delete entry $child->{queueId} from QUEUE\n"
-		  and next;
-
-	  # remove /proc entry
-	  if ( (defined $child->{queueId}) && ( $child->{queueId} ne "") && ( $child->{queueId} > 0 ) ) {
-	    my $procDir = AliEn::Util::getProcDir(undef, $child->{submitHost}, $child->{queueId});
-	    $self->info("[$cnt] Removing $procDir directory");
-	    $self->{CATALOGUE}->execute("rmdir",$procDir,"-r") or $self->info("Error deleting the directory $procDir");
-	  }
-      }
-
-      # insert master job into the archive
-      $self->{DB}->insertEntry("$self->{DB}->{QUEUEARCHIVE}",$_) or
-	  print STDERR "Expired: cannot copy entry $_->{queueId} to $self->{DB}->{QUEUEARCHIVE}\n"
-	      and return;
-
-      # delete master job from the regular queue
-      $self->{DB}->deleteJob("$_->{queueId}") or
-          print STDERR "Expired: cannot delete entry $_->{queueId} from QUEUE\n"
-              and return;
-      
-      # remove /proc entry
-      if ( (defined $_->{queueId}) && ( $_->{queueId} ne "") && ( $_->{queueId} > 0 ) ) {
-        my $procDir = AliEn::Util::getProcDir(undef, $_->{submitHost}, $_->{queueId});
-	$self->info("[$cnt] Removing $procDir directory");
-	$self->{CATALOGUE}->execute("rmdir",$procDir,"-r") or $self->info("Error deleting the directory $procDir");
-      }
-  }
-
-  undef $data;
-
-  $self->{LOGGER}->$method("Expired", "Archiving all finished jobs older than 4 weeks");
-
-  undef $data;
-
-  $data = $self->{DB}->getFieldsFromQueueEx("*","where ( ( (status='DONE') || (status='FAILED') || (status='EXPIRED') || (status like 'ERROR%') || (status='KILLED') ) && ( received < (? - 28*86540) ) ) ORDER by queueId", {bind_values=>[$now]});
-  $cnt=0;
-  foreach (@$data) {
-      $cnt++;
-      $self->info("[$cnt] Found standard job $_->{queueId} > 4 weeks finished  in status $_->{status}");
-
-      # insert master job into the archive
-      $self->{DB}->insertEntry("$self->{DB}->{QUEUEARCHIVE}",$_) or
-	  print STDERR "Expired: cannot copy entry $_->{queueId} to $self->{DB}->{QUEUEARCHIVE}\n"
-	      and next;
-
-      # delete master job from the regular queue
-      $self->{DB}->deleteJob("$_->{queueId}") or
-          print STDERR "Expired: cannot delete entry $_->{queueId} from QUEUE\n"
-              and return;
-
-      # remove /proc entry
-      if ( (defined $_->{queueId}) && ( $_->{queueId} ne "") && ( $_->{queueId} > 0 ) ) {
-        my $procDir = AliEn::Util::getProcDir(undef, $_->{submitHost}, $_->{queueId});
-	$self->info("[$cnt] Removing $procDir directory");
-	$self->{CATALOGUE}->execute("rmdir",$procDir,"-r") or $self->info("Error deleting the directory $procDir");
-      }
-  }
-
-  undef $data;
 
   $self->{LOGGER}->$method("Expired", "In checkWakesUp going back to sleep");
 
   return;
+}
+
+sub archiveJobs{
+  my $self=shift;
+  my $query=shift;
+  my $time=shift;
+  my $table=shift;
+
+  my  $now = time; 
+  $self->info("Archiving the jobs older than $time");
+  my $data= $self->{DB}->getFieldsFromQueueEx("*","where $query ORDER by queueId", {bind_values=>[$now]});
+
+  my $columns=$self->{DB}->query("describe QUEUEPROC");
+
+  my $c="";
+  my $c2="";
+  foreach my $column (@$columns){
+    $c.="$column->{Field}, ";
+    $c2.="q.$column->{Field}, ";
+  }
+  $c=~ s/, $//;
+  $c2=~ s/, $//;
+  for my $q ("insert into ${table}PROC ($c) select $c2 from ",
+	     "delete from q using" ){
+    $self->{DB}->do("$q QUEUEPROC q, QUEUE s where s.queueid=q.queueid and $query", {bind_values=>[$now]});
+  }
+
+
+  foreach my $job (@$data){
+    $self->info(" Found standard job $job->{queueId} > $time old in status $job->{status}");
+    
+    # insert master job into the archive
+    $self->{DB}->insertEntry($table,$job) or
+      print STDERR "Expired: cannot copy entry $job->{queueId} to $table\n" and next;
+    
+    # delete master job from the regular queue
+    $self->{DB}->deleteJob("$job->{queueId}") or
+      print STDERR "Expired: cannot delete entry $job->{queueId} from QUEUE\n"
+	and next;
+    
+    # remove /proc entry
+    if ( (defined $job->{queueId}) && ( $job->{queueId} ne "") && ( $job->{queueId} > 0 ) ) {
+      my $procDir = AliEn::Util::getProcDir(undef, $job->{submitHost}, $job->{queueId});
+      $self->info("   Removing $procDir directory");
+      $self->{CATALOGUE}->execute("rmdir",$procDir,"-r") or $self->info("Error deleting the directory $procDir");
+    }
+  }
+
+
+  return 1;
 }
 
 1;
