02fa7fd4d2d97c52339f890ffd5d4e8335ec0039
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2009-06-09 16:26:01 +0000

    Limiting the number of transfers per SE

--- common/trunk/lib/AliEn/Service/Broker/Transfer.pm
+++ common/trunk/lib/AliEn/Service/Broker/Transfer.pm
@@ -64,8 +64,9 @@ sub getTransferFromAgentId {
   if (!$cache){
     $self->info("Getting the jobids for jobagent '$agentId'");
     my $data=AliEn::Util::returnCacheValue($self, "WaitingTransfersFor$agentId");
+    $self->{DB}->do("update PROTOCOLS a  set current_transfers= (select count(*) from  TRANSFERS_DIRECT b where status='TRANSFERRING' and a.sename=b.destination)");
     if (! $data){
-      $data=$self->{DB}->query("select transferid as id, jdl from TRANSFERS_DIRECT where agentid=? and STATUS='WAITING' order by transferid", undef, {bind_values=>[$agentId]});
+      $data=$self->{DB}->query("select transferid as id, jdl from TRANSFERS_DIRECT join PROTOCOLS on sename=destination where agentid=? and STATUS='WAITING' and max_transfers>current_transfers order by transferid", undef, {bind_values=>[$agentId]});
     }
     $self->info("There are $#$data entries for that jobagent");
     return @$data;
