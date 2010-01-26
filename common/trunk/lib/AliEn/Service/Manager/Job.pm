97d36e38f0a78588b397fcca43d2c416f5b8c823
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2010-01-26 08:50:21 +0000

    Moving the filequota out of the jobmanager. Adding quotas when a user is created. More options to the masterSE

--- common/trunk/lib/AliEn/Service/Manager/Job.pm
+++ common/trunk/lib/AliEn/Service/Manager/Job.pm
@@ -1784,53 +1784,6 @@ sub calculateFileQuota {
   return 1;
 }
 
-sub checkFileQuota {
-  my $this = shift;
-  my $user = shift
-    or $self->{LOGGER}->error("In checkFileQuota user is missing\n")
-    and return (-1, "user is missing");
-  my $size = shift;
-	(defined $size) 
-    or $self->{LOGGER}->error("In checkFileQuota size is missing\n")
-    and return (-1, "size is missing");
-
-  $DEBUG and $self->debug(1, "In checkFileQuota user:$user, size:$size");
-  $self->info("In checkFileQuota user:$user, size:$size");
- 
-  my $array = $self->{PRIORITY_DB}->getFieldsFromPriorityEx("nbFiles, totalSize, maxNbFiles, maxTotalSize, tmpIncreasedNbFiles, tmpIncreasedTotalSize", "where user like '$user'")
-    or $self->{LOGGER}->error("Failed to getting data from PRIORITY table")
-    and return (-1, "Failed to getting data from PRIORITY table");
-  $array->[0] or $self->{LOGGER}->error("User $user not exist")
-    and return (-1, "User $user not exist in PRIORITY table");
- 
-  my $nbFiles = $array->[0]->{'nbFiles'};
-  my $maxNbFiles = $array->[0]->{'maxNbFiles'};
-  my $tmpIncreasedNbFiles = $array->[0]->{'tmpIncreasedNbFiles'};
-  my $totalSize = $array->[0]->{'totalSize'};
-  my $maxTotalSize = $array->[0]->{'maxTotalSize'};
-  my $tmpIncreasedTotalSize = $array->[0]->{'tmpIncreasedTotalSize'};
- 
-  $DEBUG and $self->debug(1, "size: $size");
-  $DEBUG and $self->debug(1, "nbFile: $nbFiles/$tmpIncreasedNbFiles/$maxNbFiles");
-  $DEBUG and $self->debug(1, "totalSize: $totalSize/$tmpIncreasedTotalSize/$maxTotalSize");
-  $self->info("nbFile: $nbFiles/$tmpIncreasedNbFiles/$maxNbFiles");
-  $self->info("totalSize: $totalSize/$tmpIncreasedTotalSize/$maxTotalSize");
-
-    if ($nbFiles + $tmpIncreasedNbFiles + 1 > $maxNbFiles) {
-    $self->info("In checkFileQuota $user: Not allowed for nbFiles overflow");
-    return { allowed=>0, message=>"DENIED: You're trying to upload 1 file. That exceeds your limit." };
-  }
-
-  if ($size + $totalSize + $tmpIncreasedTotalSize > $maxTotalSize) {
-    $self->info("In checkFileQuota $user: Not allowed for totalSize overflow");
-    return { allowed=>0, message=>"DENIED: You've trying to upload a file which size is $size. That exceeds your limit." };
-  }
-
-	$self->{PRIORITY_DB}->do("update PRIORITY set tmpIncreasedNbFiles=tmpIncreasedNbFiles+1, tmpIncreasedTotalSize=tmpIncreasedTotalSize+$size where user like '$user'") or $self->info("failed to increase tmpIncreasedNbFile and tmpIncreasedTotalSize");
-
-  $self->info("In checkFileQuota $user: Allowed");
-  return { allowed=>1 };
-}
 
 #_______________________________________________________________________________________________________________________
 
