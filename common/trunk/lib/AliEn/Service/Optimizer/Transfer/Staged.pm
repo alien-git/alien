b644e0ce943bc3461eb0f2c8d5facce726931362
lolass <lolass@4525493e-7705-40b1-a816-d608a930855b>
2010-11-26 16:12:26 +0000

    update

--- common/trunk/lib/AliEn/Service/Optimizer/Transfer/Staged.pm
+++ common/trunk/lib/AliEn/Service/Optimizer/Transfer/Staged.pm
@@ -6,6 +6,7 @@ use vars qw (@ISA);
 use AliEn::Service::Optimizer::Transfer;
 use AliEn::Service;
 use Classad;
+use POSIX;
 
 push (@ISA, "AliEn::Service::Optimizer::Transfer");
 	
@@ -21,47 +22,61 @@ sub checkWakesUp {
   $silent and $method="debug" and push @silentData, 1;
   $self->$method(@silentData, "Check STAGED transfers. Checking if there is anything to do");
 
-  my $transfers =$self->{DB}->query("SELECT transferId,pfn,ctime FROM TRANSFERS_DIRECT where status='STAGED'");
+  my $transfers =$self->{DB}->query("SELECT transferId,pfn,ctime,retrytime,maxtime FROM TRANSFERS_DIRECT where status='STAGED'");
 
   defined $transfers
     or $self->{LOGGER}->warning( "TransferOptimizer", "In checkTransferRequirements in Staged.pm: error during execution of database query" )
       and return;
 
   $self->info( "There are ".($#{$transfers} +1)." transfers stuck in STAGED");
-  #my $maxTime = 32578; #waiting as maximum for 8 hours
-  my $maxTime = 100;
-
-#  my $retryTime =10;
-
 
 #   $self->{TRANSFERLOG}->putlog($id, "STATUS", 'checkStaged. Transfer STAGED. Optimizer trying to retry it');
 
   foreach my $transfer (@$transfers){
 	my $pfn = $transfer->{pfn};
 	my $id = $transfer->{transferId};
-	my $retryTime = $transfer->{ctime};
+	my $maxTime = $transfer->{maxtime};
+	my $retryTime;
+	my @x = split(/\D/, $transfer->{ctime});
+	$x[0] -= 1900;
+	$x[1] -= 1;
+	my $initTime = strftime("%s", reverse @x);
+	$self->$method(@silentData, "cime is  $initTime ");
+	my $actualTime = time ();
+        
+	if (defined($transfer->{retrytime}) and $transfer->{retrytime} ne 'NULL' and $transfer->{retrytime} != 0) {
+		$retryTime = $transfer->{retrytime};
+	} else {
+		$retryTime = time()-1;
+	}
+
 	$self->{TRANSFERLOG}->putlog($id, "STATUS", 'checkStaged. Transfer STAGED. Optimizer trying to retry it');
-	$self->$method(@silentData, "Retrying transfer $id, pfn = $pfn");
-	if ($retryTime < $maxTime){
-		#my $stageQueryOut = system("stager_qry -M ".$pfn) ; # stage-query command here. Binary value
-		 $self->$method(@silentData, "retry Time less than Maxtime  $retryTime < $maxTime");
-		my $stageQueryOut = 1;
+	$self->$method(@silentData, "Retrying transfer $id, retrytime $retryTime, actual time $actualTime, maxtime $maxTime, init time ctime = $initTime");
+	if ($retryTime < $maxTime && $retryTime<=$actualTime){
+		my $stageQueryOut = system("stager_qry -M ".$pfn) ; # stage-query command here. Binary value
+		$self->$method(@silentData, "retryTime less than Maxtime  $retryTime < $maxTime. StageQueryout stager_qry -M $pfn ".$stageQueryOut);
+		my $stageQueryOut = 0;
 		if ($stageQueryOut) {# stage-query does not succed
-			$retryTime = $retryTime * 2;
-		        $self->{DB}->updateTransfer($id,{ctime=>$retryTime});
-			$self->$method(@silentData, "Retrying transfer $id failed. Sleep for $retryTime");
-			sleep $retryTime;
+			$retryTime = $actualTime + ($retryTime - $initTime)* 2;
+			$self->$method(@silentData, "retryTime is  $retryTime ");
+			my $query = {ctime=>$actualTime};
+			$query->{retrytime} = $retryTime;
+			$query->{maxtime} = $maxTime;
+		        $self->{LOGGER}->debug("FTD","Update Transfer $id with ".$query->{maxtime}.", retryTime ".$query->{retrytime}." and ".$query->{ctime});
+			$self->{SOAP}->CallSOAP("Manager/Transfer","changeStatusTransfer",$id, 'STAGED',"ALIEN_SOAP_RETRY",$query);
+			$self->$method(@silentData, "Retrying transfer $id failed. Sleep until $retryTime");
 		}
 		else {
 			$self->$method(@silentData, "Retrying transfer $id done, changing status to WAITING");
-			$self->{DB}->updateTransfer($id,{status=>"WAITING"});
+			$self->{SOAP}->CallSOAP("Manager/Transfer","changeStatusTransfer",$id, 'WAITING',"ALIEN_SOAP_RETRY");
+			#$self->{DB}->updateTransfer($id,{status=>"WAITING"});
 			return 0;
 		}
 	}
-	else {
- # 	$self->{LOGGER}->error("Error trying to transfer '$transfer->{jdl}'");
-        	$self->{TRANSFERLOG}->putlog($id, "STATUS", 'Transfer FAILED');
-		$self->{DB}->updateTransfer($id,{status=>"FAILED"});
+	elsif ($retryTime > $maxTime ) {
+        	$self->$method(@silentData, "Retrying transfer $id done, changing status to FAILED");
+		$self->{SOAP}->CallSOAP("Manager/Transfer","changeStatusTransfer",$id, 'FAILED',"ALIEN_SOAP_RETRY");
+		#$self->{DB}->updateTransfer($id,{status=>"FAILED"});
 	}
   }
 	return;
