2e48a4bbd5d150ff3e69f1558e21037f0d947b6e
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2010-12-20 18:49:03 +0000

    Doing the mirror -u

--- common/trunk/lib/AliEn/UI/Catalogue/LCM.pm
+++ common/trunk/lib/AliEn/UI/Catalogue/LCM.pm
@@ -991,7 +991,7 @@ sub mirror {
   if ($opt->{u}){
     $self->info("Making sure that the file is not in that SE");
     my $nopt="";
-    $opt->{r} and $nopt.="r";
+    $opt->{r} and $nopt.="-r";
     my @info=$self->{CATALOG}->f_whereis($nopt, $realLfn)
       or $self->info("Error getting the info from $realLfn") and return;
     if (grep (/^$se$/i, @info)){
