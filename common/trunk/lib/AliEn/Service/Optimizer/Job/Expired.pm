75894f2db01ab1525b98e492cf52ab06c96e4a35
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2011-01-31 11:03:37 +0000

    Moving old jobs to the expired table

--- common/trunk/lib/AliEn/Service/Optimizer/Job/Expired.pm
+++ common/trunk/lib/AliEn/Service/Optimizer/Job/Expired.pm
@@ -4,9 +4,9 @@ use strict;
 
 use AliEn::Service::Optimizer::Job;
 use vars qw(@ISA);
+use POSIX qw(strftime);
+
 push (@ISA, "AliEn::Service::Optimizer::Job");
-
-
 sub checkWakesUp {
   my $self=shift;
   my $silent=shift;
@@ -17,20 +17,21 @@ sub checkWakesUp {
   $self->{SLEEP_PERIOD}=3600;
   $self->{LOGGER}->$method("Expired", "In checkWakesUp .... optimizing the QUEUE table ...");
    
-
+  my $time = strftime "%Y-%m-%d %H:%M:%S",  localtime(time - 240*3600 );
   #Completed master Jobs older than 10 days are moved to the archive
   $self->archiveJobs("where (( status in ('DONE','FAILED','EXPIRED') || status like 'ERROR%'  ) 
-                      and ( mtime < addtime(now(), '-10 00:00:00')  and split=0) )", "10 days" ,$self->{DB}->{QUEUEARCHIVE});
+                      and ( mtime < '$time')  and split=0) ", "10 days in final state" ,$self->{DB}->{QUEUEARCHIVE});
 
 
 
 # #This is to archive the subjobs 'select count(*) from QUEUE q left join QUEUE q2
   $self->archiveJobs("left join QUEUE q2 on q.split=q2.queueid where 
-                      q.split!=0 and q2.queueid is null and q.mtime<addtime(now(), '-10 00:00:00')", 
-                      " subjobs ", $self->{DB}->{QUEUEARCHIVE});
+                      q.split!=0 and q2.queueid is null and q.mtime<'$time'", 
+                      "10 days without subjobs ", $self->{DB}->{QUEUEARCHIVE});
   
     #This is slightly more than ten days, and we move it to another table
-  $self->archiveJobs("where mtime < addtime(now(), '-10 00:00:00') and split=0", "10 days","QUEUEEXPIRED" );
+  $time = strftime "%Y-%m-%d %H:%M:%S",  localtime(time - 2*240*3600 );
+  $self->archiveJobs("where mtime < '$time' and split=0", "20 days in any state","QUEUEEXPIRED" );
 
 
 
@@ -47,45 +48,13 @@ sub archiveJobs{
 
   
   $self->info("Archiving the jobs older than $time");
-  my $data= $self->{DB}->getFieldsFromQueueEx("*","q $query ORDER by q.queueId");
 
-  my $columns=$self->{DB}->query("describe QUEUEPROC");
+  my $done=$self->{DB}->do("insert into ${table}PROC select p.* from QUEUE q join QUEUEPROC p using (queueid) $query");
+  my $done2=$self->{DB}->do("insert into ${table} select q.* from QUEUE q $query");
+  my $done3=$self->{DB}->do("delete from p using  QUEUE q join QUEUEPROC p using (queueid) $query");
+  my $done4=$self->{DB}->do("delete from q using QUEUE q $query");
 
-  my $c="";
-  my $c2="";
-  foreach my $column (@$columns){
-    $c.="$column->{Field}, ";
-    $c2.="s.$column->{Field}, ";
-  }
-  $c=~ s/, $//;
-  $c2=~ s/, $//;
-  for my $q ("insert into ${table}PROC ($c) select $c2 from ",
-	     "delete from s using" ){
-    $self->{DB}->do("$q QUEUEPROC s, QUEUE q $query and s.queueid=q.queueid");
-  }
-
-
-  foreach my $job (@$data){
-    $self->info(" Found standard job $job->{queueId} > $time old in status $job->{status}");
-    
-    # insert master job into the archive
-    $self->{DB}->insertEntry($table,$job) or
-      print STDERR "Expired: cannot copy entry $job->{queueId} to $table\n" and next;
-    
-    # delete master job from the regular queue
-    $self->{DB}->deleteJob("$job->{queueId}") or
-      print STDERR "Expired: cannot delete entry $job->{queueId} from QUEUE\n"
-	and next;
-    
-#    # remove /proc entry
-#    if ( (defined $job->{queueId}) && ( $job->{queueId} ne "") && ( $job->{queueId} > 0 ) ) {
-#      my $procDir = AliEn::Util::getProcDir(undef, $job->{submitHost}, $job->{queueId});
-#      $self->info("   Removing $procDir directory");
-#      $self->{CATALOGUE}->execute("rmdir",$procDir,"-r") or $self->info("Error deleting the directory $procDir");
-#    }
-  }
-
-
+  $self->info("AT THE END, WE HAVE $done and $done2 and $done3 and $done4");
   return 1;
 }
 
