7ddbf0373eebe359600dfc4acfe509037dfb8573
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2011-02-09 22:33:38 +0000

    Renaming ERROR_Rto ERROR_RE following Costin request

--- common/trunk/lib/AliEn/Util.pm
+++ common/trunk/lib/AliEn/Util.pm
@@ -71,7 +71,7 @@ sub getJobUserByDB {
 }
 
 sub JobStatus {
-  return ['ASSIGNED','DONE','DONE_WARN','ERROR_A','ERROR_I','ERROR_E','ERROR_IB','ERROR_M','ERROR_R','ERROR_S','ERROR_SPLT', 'ERROR_SV','ERROR_V','ERROR_VN','EXPIRED','FAILED','FORCEMERGE','IDLE','INSERTING','INTERACTIV','KILLED','MERGING','QUEUED','RUNNING','SAVING','SAVED','SAVED_WARN', 'SPLIT','SPLITTING','STARTED','WAITING','ZOMBIE', 'ERROR_VT', 'TO_STAGE', 'STAGING', 'A_STAGED', 'OVER_WAITING'];
+  return ['ASSIGNED','DONE','DONE_WARN','ERROR_A','ERROR_I','ERROR_E','ERROR_IB','ERROR_M','ERROR_RE','ERROR_S','ERROR_SPLT', 'ERROR_SV','ERROR_V','ERROR_VN','EXPIRED','FAILED','FORCEMERGE','IDLE','INSERTING','INTERACTIV','KILLED','MERGING','QUEUED','RUNNING','SAVING','SAVED','SAVED_WARN', 'SPLIT','SPLITTING','STARTED','WAITING','ZOMBIE', 'ERROR_VT', 'TO_STAGE', 'STAGING', 'A_STAGED', 'OVER_WAITING'];
 }
 
 # mapping between job status as text and number
@@ -93,9 +93,9 @@ my $ml_status ={'INSERTING' => 1,
 		'TO_STAGE'=>17, 'A_STAGED'=>18,'STAGING'=>19,
 		'OVER_WAITING'=>21,
 		'ERROR_A' => -1, 'ERROR_I' => -2, 'ERROR_E' => -3, 'ERROR_IB' => -4, 
-		'ERROR_M' => -5, 'ERROR_R' => -6, 'ERROR_S' => -7, 'ERROR_SPLT' => -8, 
+		'ERROR_M' => -5, , 'ERROR_S' => -7, 'ERROR_SPLT' => -8, 
 		'ERROR_SV' => -9, 'ERROR_V' => -10, 'ERROR_VN' => -11, 'EXPIRED' => -12,
-		'FAILED' => -13, 'KILLED' => -14, 'ZOMBIE' => -15, 'ERROR_VT' => -16};
+		'FAILED' => -13, 'KILLED' => -14, 'ZOMBIE' => -15, 'ERROR_VT' => -16, 'ERROR_RE'=>-17};
 
 # convert a job status to a number to be used in MonaLisa
 sub statusForML {
