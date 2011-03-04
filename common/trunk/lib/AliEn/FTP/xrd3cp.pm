883a0e336d5e04af14d12add3583f93c22fba533
costin <costing@users.noreply.github.com>
2011-03-04 12:11:11 +0000

    Disable retrieving log in case of failure, this crashes the source

--- common/trunk/lib/AliEn/FTP/xrd3cp.pm
+++ common/trunk/lib/AliEn/FTP/xrd3cp.pm
@@ -51,28 +51,29 @@ sub copy {
 
   $self->debug(2, "Exit code: $com_exit_value, Returned output: $output");
 
-  if($com_exit_value ne 0) {
-     # &xferuuid=2d20aade-5859-11df-9e4b-001e0bd3f44c&xfercmd=preparetoput located at 128.142.216.111:1094
-
-     $self->info("Error doing the xrd3cp $args. Trying to retrieve additional log info.",1);
-     $self->info("Exit code not equal to zero. Something went wrong with xrdcp!! Exit code: $com_exit_value, Returned output: $output",1);
-     $output=~ /xferuuid=([0-9a-fA-F\-]+)/;
-     my $xferuuid = $1;
-     (AliEn::Util::isValidGUID($xferuuid))
-       or $self->info("Error doing the xrd3cp $args. Not possible to retrieve additional log info due to invalid xferuuid",1) and return;
-
-     my @logargs= ("-l $xferuuid $sEnvelope->{turl} $tEnvelope->{turl}  \"authz=$sourceEnvelope\" \"authz=$targetEnvelope\" ");  
-     my $logoutput = `xrd3cp $args  2>&1 ` or $self->info("Error: Error doing the xrd3cp $args",1) and return;
-     #my $com_exit_value=$? >> 8;
-     $logoutput =~ s/\s+$//;
-     $self->info("Additional log info from xrd3cp for xferuuid ($xferuuid): $logoutput",1);
-     if ($output =~ /file is not online/) { 
-          $self->info("Error doing the xrd3cp $args. File is not online",1);
-          return 3;
-     }
-
-     return;
-  }
+#### Disabled per Andreas' request since retrieving the logs crashes the source
+#  if($com_exit_value ne 0) {
+#     # &xferuuid=2d20aade-5859-11df-9e4b-001e0bd3f44c&xfercmd=preparetoput located at 128.142.216.111:1094
+#
+#     $self->info("Error doing the xrd3cp $args. Trying to retrieve additional log info.",1);
+#     $self->info("Exit code not equal to zero. Something went wrong with xrdcp!! Exit code: $com_exit_value, Returned output: $output",1);
+#     $output=~ /xferuuid=([0-9a-fA-F\-]+)/;
+#     my $xferuuid = $1;
+#     (AliEn::Util::isValidGUID($xferuuid))
+#       or $self->info("Error doing the xrd3cp $args. Not possible to retrieve additional log info due to invalid xferuuid",1) and return;
+#
+#     my @logargs= ("-l $xferuuid $sEnvelope->{turl} $tEnvelope->{turl}  \"authz=$sourceEnvelope\" \"authz=$targetEnvelope\" ");  
+#     my $logoutput = `xrd3cp $args  2>&1 ` or $self->info("Error: Error doing the xrd3cp $args",1) and return;
+#     #my $com_exit_value=$? >> 8;
+#     $logoutput =~ s/\s+$//;
+#     $self->info("Additional log info from xrd3cp for xferuuid ($xferuuid): $logoutput",1);
+#     if ($output =~ /file is not online/) { 
+#          $self->info("Error doing the xrd3cp $args. File is not online",1);
+#          return 3;
+#     }
+#
+#     return;
+#  }
 
   $self->info("The transfer worked!!");
   return 1;
