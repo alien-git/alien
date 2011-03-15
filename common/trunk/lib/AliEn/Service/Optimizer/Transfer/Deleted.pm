dae1fb7dcb5d69780d081b862615ed980d20c3f5
almudena <almudena@4525493e-7705-40b1-a816-d608a930855b>
2011-03-15 15:58:38 +0000

    compatibility with Oracle

--- common/trunk/lib/AliEn/Service/Optimizer/Transfer/Deleted.pm
+++ common/trunk/lib/AliEn/Service/Optimizer/Transfer/Deleted.pm
@@ -21,7 +21,10 @@ sub checkWakesUp {
   foreach my $rtempHost (@$rhosts) {
     my ($db2, $extra)=$self->{CATALOGUE}->{CATALOG}->{DATABASE}->{LFN_DB}->reconnectToIndex( $rtempHost->{hostIndex}, "", $rtempHost );
     $db2 or $self->info("Error reconecting to $rtempHost->{hostIndex}") and next;
-    my $d=$db2->query("select entryId,pfn,sename from TODELETE join SE using (senumber) order by entryId limit 100");
+    my $query = "select entryId,pfn,sename from TODELETE join SE using (senumber) order by entryId";
+    $query = $db2->paginate($query,100,0);
+    my $d=$db2->query($query);
+    #my $d=$db2->query("select entryId,pfn,sename from TODELETE join SE using (senumber) order by entryId limit 100");
     my $max=0;
     foreach my $entry (@$d){
       $self->info("Inserting a new request to delete");
