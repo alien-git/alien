972ed85e16da4a25c0097c84db4436bb2232be94
almudena <almudena@4525493e-7705-40b1-a816-d608a930855b>
2011-03-16 10:44:22 +0000

    format of date for Oracle

--- common/trunk/lib/AliEn/Service/Optimizer/Job/Expired.pm
+++ common/trunk/lib/AliEn/Service/Optimizer/Job/Expired.pm
@@ -16,22 +16,25 @@ sub checkWakesUp {
 
   $self->{SLEEP_PERIOD}=3600;
   $self->{LOGGER}->$method("Expired", "In checkWakesUp .... optimizing the QUEUE table ...");
-   
-  my $time = strftime "%Y-%m-%d %H:%M:%S",  localtime(time - 240*3600 );
+ 
+  my $time = strftime "%Y-%m-%d %H:%M:%S", localtime(time - 240*3600 );
+ # my $time =  localtime(time - 240*3600 );
+  my $mtime = "q.mtime";
+# my $month = qw(Jan Feb Mar Avr May Jun Jul Aug Sep Oct Nov Dec)($ltime[4]);
+  $self->{DB}->{DRIVER}=~ /Oracle/i and $mtime ="to_char(q.mtime,\'YY-MM-DD HH24:Mi:ss\')";
+ # my $time =  (time - 240*3600 );
   #Completed master Jobs older than 10 days are moved to the archive
-  $self->archiveJobs("where (( status in ('DONE','FAILED','EXPIRED') or status like 'ERROR%'  ) 
-                      and ( mtime < '$time')  and split=0) ", "10 days in final state" ,$self->{DB}->{QUEUEARCHIVE});
+  $self->archiveJobs("where (( status in ('DONE','FAILED','EXPIRED') or status like 'ERROR%') and ( $mtime < '$time')  and split=0) ", "10 days in final state" ,$self->{DB}->{QUEUEARCHIVE});
 
 
 
 # #This is to archive the subjobs 'select count(*) from QUEUE q left join QUEUE q2
-  $self->archiveJobs("left join QUEUE q2 on q.split=q2.queueid where 
-                      q.split!=0 and q2.queueid is null and q.mtime<'$time'", 
+  $self->archiveJobs("left join QUEUE q2 on q.split=q2.queueid where q.split!=0 and q2.queueid is null and $mtime<'$time'", 
                       "10 days without subjobs ", $self->{DB}->{QUEUEARCHIVE});
   
     #This is slightly more than ten days, and we move it to another table
   $time = strftime "%Y-%m-%d %H:%M:%S",  localtime(time - 2*240*3600 );
-  $self->archiveJobs("where mtime < '$time' and split=0", "20 days in any state","QUEUEEXPIRED" );
+  $self->archiveJobs("where $mtime < '$time' and split=0", "20 days in any state","QUEUEEXPIRED" );
 
 
 
@@ -50,13 +53,13 @@ sub archiveJobs{
   $self->info("Archiving the jobs older than $time");
   
   
-  $self->{DB}->do("truncate TMPID");
+  $self->{DB}->do("truncate table TMPID");
   my $jobs=$self->{DB}->do("insert into TMPID select q.queueid from QUEUE q $query ");
   $self->info("There are $jobs expired jobs");
   ( $jobs and $jobs !~ /0E0/ ) or return 1;
   
-  my $columns=$self->{DB}->describe("$table");
-  my $colQ=$self->{DB}->describe("QUEUEPROC");
+  my $columns=$self->{DB}->describeTable("$table");
+  my $colQ=$self->{DB}->describeTable("QUEUEPROC");
   my $c="";
   my $c2="";
   my $c3="";
