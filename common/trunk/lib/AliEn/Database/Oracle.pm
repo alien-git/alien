fb32122c1ef46f66dc8d54e52ed35a4499abd6cf
almudena <almudena@4525493e-7705-40b1-a816-d608a930855b>
2011-10-20 16:22:16 +0000

    change in the reserved words

--- common/trunk/lib/AliEn/Database/Oracle.pm
+++ common/trunk/lib/AliEn/Database/Oracle.pm
@@ -520,11 +520,11 @@ sub _queryDB {
 
   $self->_pingReconnect or return;
   $stmt =~
-s/(\,)\s*(size|user|time|current|validate|date|file)\s*(\,)/$1."\"".uc($2)."\"".$3/ieg;
+s/(\,|\.)\s*(size|user|time|current|validate|date|file)\s*(\,|\))/$1."\"".uc($2)."\"".$3/ieg;
   $stmt =~
 s/(\()\s*(size|user|time|current|validate|date|file)\s*(\))/$1."\"".uc($2)."\"".$3/ieg;
   $stmt =~
-s/(\s+)(size|user|time|current|validate|date|file)(\s+|\=)/$1."\"".uc($2)."\"".$3/ieg;
+s/(\s+)(size|user|time|current|validate|date|file)(\s+|\=|\))/$1."\"".uc($2)."\"".$3/ieg;
   my $arrRef;
   my $execute;
   my @bind;
@@ -643,11 +643,11 @@ sub _do {
 
   # if($stmt =~ m/^insert/i ){ $options->{zero_length}=0;}
   $stmt =~
-s/(\,)\s*(size|user|time|current|validate|date|file)\s*(\,)/$1."\"".uc($2)."\"".$3/ieg;
+s/(\,|\.)\s*(size|user|time|current|validate|date|file)\s*(\,|\))/$1."\"".uc($2)."\"".$3/ieg;
   $stmt =~
 s/(\()\s*(size|user|time|current|validate|date|file)\s*(\))/$1."\"".uc($2)."\"".$3/ieg;
   $stmt =~
-s/\W(\s+)(size|user|time|current|validate|date|file)(\s+|\=)/$1."\"".uc($2)."\"".$3/ieg;
+s/\W(\s+)(size|user|time|current|validate|date|file)(\s+|\=|\))/$1."\"".uc($2)."\"".$3/ieg;
   my $oldAlarmValue = $SIG{ALRM};
   local $SIG{ALRM} = \&_timeout;
   my $check = $options->{zero_length};
