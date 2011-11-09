de5bccf7629ef8113b453089c7a422c2c44cad8a
dgoyal <dgoyal@4525493e-7705-40b1-a816-d608a930855b>
2011-11-09 16:31:05 +0000

    some more fixes in the script

--- admin/trunk/scripts/newstruc.pl
+++ admin/trunk/scripts/newstruc.pl
@@ -27,17 +27,18 @@ my $guidIndex  = $db->query("SELECT tableName FROM GUIDINDEX");
 my $table;
 my $chk=0;
 
-$db->do("DROP TABLE IF EXISTS USERS,GROUPS1");
+#=cut1
+$db->do("DROP TABLE IF EXISTS USERS,GRPS");
 $db->do("CREATE TABLE USERS (uId MEDIUMINT not null PRIMARY KEY AUTO_INCREMENT, user varchar(20) not null UNIQUE ) CHARACTER SET latin1 COLLATE latin1_general_cs");
-$db->do("CREATE TABLE GROUPS1 (gId MEDIUMINT not null PRIMARY KEY AUTO_INCREMENT, user varchar(20) not null UNIQUE ) CHARACTER SET latin1 COLLATE latin1_general_cs");
-print "Created 2 new tables: USERS and GROUPS1\n";
+$db->do("CREATE TABLE GRPS (gId MEDIUMINT not null PRIMARY KEY AUTO_INCREMENT, user varchar(20) not null UNIQUE ) CHARACTER SET latin1 COLLATE latin1_general_cs");
+print "Created 2 new tables: USERS and GRPS\n";
 print "\n".scalar(localtime(time))."\n";
 
 #INDEXTABLE
 foreach my $row (@$indexTable) {
   $table="L".$row->{tableName}."L";
   $db->do("INSERT IGNORE INTO USERS (user) (SELECT DISTINCT owner from $table)");
-  $db->do("INSERT IGNORE INTO GROUPS1 (user) (SELECT DISTINCT gowner from $table)");
+  $db->do("INSERT IGNORE INTO GRPS (user) (SELECT DISTINCT gowner from $table)");
   #$db->do("ALTER TABLE $table DROP COLUMN uId, DROP COLUMN gId");
 }
 print "Doing the alteration in L#L tables\n";
@@ -47,15 +48,21 @@ my $status=0;
 my $collation='latin1_general_cs';
 foreach my $row (@$indexTable) {
   $table="L".$row->{tableName}."L";
-  $db->do("ALTER TABLE $table ADD (ownerId MEDIUMINT , gownerId MEDIUMINT)");
+  print "$table\n";
+  $db->do("ALTER TABLE $table ADD (ownerId MEDIUMINT , gownerId MEDIUMINT), ADD FOREIGN KEY (ownerId) REFERENCES USERS(uId),ADD FOREIGN KEY (gownerId) REFERENCES GRPS(gId)" , {timeout=>[60000]} );
   $status=$db->do("select 1 from information_schema.tables where table_name='$table' and table_collation='$collation'");
   ($status) or $status=0;
-  if($status==0)
-  { $db->do("ALTER TABLE $table COLLATE latin1_general_cs");}
+  if($status==0){
+    print "Changing the collation of table $table\n";
+    $db->do("ALTER TABLE $table CONVERT TO CHARACTER SET latin1 COLLATE latin1_general_cs");
+    $db->do("ALTER TABLE $table COLLATE latin1_general_cs");
+  }
 
-  $db->do("UPDATE $table JOIN USERS ON $table.owner=USERS.user SET $table.ownerId=USERS.uId") or $db->do("UPDATE $table JOIN USERS ON BINARY $table.owner=USERS.user SET $table.ownerId=USERS.uId") ;
-  $db->do("UPDATE $table JOIN GROUPS1 ON $table.gowner=GROUPS1.user SET $table.gownerId=GROUPS1.gId") or $db->do("UPDATE $table JOIN GROUPS1 ON BINARY $table.gowner=GROUPS1.user SET $table.gownerId=GROUPS1.gId");
-  $db->do("ALTER TABLE $table DROP COLUMN owner, DROP COLUMN gowner");
+  $db->do("UPDATE $table JOIN USERS ON $table.owner=USERS.user JOIN GRPS ON $table.gowner=GRPS.user SET $table.ownerId=USERS.uId, $table.gownerId=GRPS.gId",{timeout=>[60000]} );
+  #  or $db->do("UPDATE $table JOIN USERS ON BINARY $table.owner=USERS.user SET $table.ownerId=USERS.uId") ;
+  # $db->do("UPDATE $table JOIN GRPS ON $table.gowner=GRPS.user SET $table.gownerId=GRPS.gId") 
+  #  or $db->do("UPDATE $table JOIN GRPS ON BINARY $table.gowner=GRPS.user SET $table.gownerId=GRPS.gId");
+  $db->do("ALTER TABLE $table DROP COLUMN owner, DROP COLUMN gowner",{timeout=>[60000]});
 }
 print "New Changes made successfully !!!\n";
 print "\n".scalar(localtime(time))."\n";
@@ -65,32 +72,41 @@ print "\n".scalar(localtime(time))."\n";
 foreach my $row (@$guidIndex) {
   $table="G".$row->{tableName}."L";
   $db->do("INSERT IGNORE INTO USERS (user) (SELECT DISTINCT owner from $table)");
-  $db->do("INSERT IGNORE INTO GROUPS1 (user) (SELECT DISTINCT gowner from $table )");
+  $db->do("INSERT IGNORE INTO GRPS (user) (SELECT DISTINCT gowner from $table )");
 }
 print "Doing the alteration in G#L tables\n";
 print "\n".scalar(localtime(time))."\n";
 foreach my $row (@$guidIndex) {
   $table="G".$row->{tableName}."L";
-  $db->do("ALTER TABLE $table ADD (ownerId MEDIUMINT , gownerId MEDIUMINT)");
+  print "$table\n";
+  $db->do("ALTER TABLE $table ADD (ownerId MEDIUMINT , gownerId MEDIUMINT), ADD FOREIGN KEY (ownerId) REFERENCES USERS(uId),ADD FOREIGN KEY (gownerId) REFERENCES GRPS(gId)" ,{timeout=>[60000]});
   $status=$db->do("select 1 from information_schema.tables where table_name='$table' and table_collation='$collation'");
   ($status) or $status=0;
-  if($status==0)
-  { $db->do("ALTER TABLE $table COLLATE latin1_general_cs");}
+  if($status==0){
+    print "Changing the collation of table $table\n";
+    $db->do("ALTER TABLE $table CONVERT TO CHARACTER SET latin1 COLLATE latin1_general_cs");
+    $db->do("ALTER TABLE $table COLLATE latin1_general_cs");
+  }
 
-  $db->do("UPDATE $table join USERS ON $table.owner=USERS.user SET $table.ownerId= USERS.uId");
-  $db->do("UPDATE $table join GROUPS1 ON $table.gowner=GROUPS1.user SET $table.gownerId= GROUPS1.gId");
-  $db->do("ALTER TABLE $table DROP COLUMN owner, DROP COLUMN gowner");
+  $db->do("UPDATE $table JOIN USERS ON $table.owner=USERS.user JOIN GRPS ON $table.gowner=GRPS.user SET $table.ownerId=USERS.uId, $table.gownerId=GRPS.gId" ,{timeout=>[60000]}); 
+  # or $db->do("UPDATE $table JOIN USERS ON BINARY $table.owner=USERS.user SET $table.ownerId=USERS.uId") ;
+  # $db->do("UPDATE $table JOIN GRPS ON $table.gowner=GRPS.user SET $table.gownerId=GRPS.gId");
+  # or $db->do("UPDATE $table JOIN GRPS ON BINARY $table.gowner=GRPS.user SET $table.gownerId=GRPS.gId");
+  # $db->do("UPDATE $table join USERS ON $table.owner=USERS.user SET $table.ownerId= USERS.uId");
+  # $db->do("UPDATE $table join GRPS ON $table.gowner=GRPS.user SET $table.gownerId= GRPS.gId");
+  $db->do("ALTER TABLE $table DROP COLUMN owner, DROP COLUMN gowner" ,{timeout=>[60000]});
 }
 print "New Changes made successfully !!!\n";
 print "\n".scalar(localtime(time))."\n";
 
 
 print "Updating the GROUPS table to UGMAP with uId and gId & DROP columns Username and Groupname\n";
-$db->do("ALTER TABLE GROUPS ADD (uId MEDIUMINT ,gId MEDIUMINT)");
-$db->do("UPDATE GROUPS join USERS ON USERS.user=GROUPS.Username SET GROUPS.uId=USERS.uId ");
-$db->do("UPDATE GROUPS join GROUPS1 ON GROUPS1.user=GROUPS.Groupname SET GROUPS.gId=GROUPS1.gId ");
-#$db->do("ALTER TABLE GROUPS RENAME UGMAP");
-#$db->do("ALTER TABLE UGMAP DROP COLUMN Username, DROP COLUMN Groupname");
+$db->do("ALTER TABLE GROUPS ADD (Userid MEDIUMINT ,Groupid MEDIUMINT)");
+$db->do("UPDATE GROUPS join USERS ON USERS.user=GROUPS.Username SET GROUPS.Userid=USERS.uId ");
+$db->do("UPDATE GROUPS join GRPS ON GRPS.user=GROUPS.Groupname SET GROUPS.Groupid=GRPS.gId ");
+$db->do("ALTER TABLE GROUPS RENAME UGMAP");
+$db->do("ALTER TABLE UGMAP DROP COLUMN Username, DROP COLUMN Groupname");
 
 
 print "\n".scalar(localtime(time))."\n";
+#=cut
