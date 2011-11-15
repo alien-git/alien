830c49bb97717b27f71d49625516586e9fb04ddc
dgoyal <dgoyal@4525493e-7705-40b1-a816-d608a930855b>
2011-11-15 12:43:31 +0000

    Adding a new script for synchronization of database modifications and a few fixes

--- admin/trunk/scripts/sync_newstruc.pl
+++ admin/trunk/scripts/sync_newstruc.pl
@@ -27,10 +27,10 @@ my $guidIndex  = $db->query("SELECT tableName FROM GUIDINDEX");
 my $table;
 my $chk=0;
 
-#=cut1
 $db->do("DROP TABLE IF EXISTS USERS,GRPS");
 $db->do("CREATE TABLE USERS (uId MEDIUMINT UNSIGNED  not null PRIMARY KEY AUTO_INCREMENT, user varchar(20) not null UNIQUE ) CHARACTER SET latin1 COLLATE latin1_general_cs");
 $db->do("CREATE TABLE GRPS (gId MEDIUMINT UNSIGNED  not null PRIMARY KEY AUTO_INCREMENT, user varchar(20) not null UNIQUE ) CHARACTER SET latin1 COLLATE latin1_general_cs");
+#$db->do("CREATE SCHEMA IF NOT EXISTS alice_users_sync");
 print "Created 2 new tables: USERS and GRPS\n";
 print "\n".scalar(localtime(time))."\n";
 
@@ -45,11 +45,28 @@ print "Doing the alteration in L#L tables\n";
 print "\n".scalar(localtime(time))."\n";
 
 my $status=0;
+my $update_status=0;
+my $count1=0;
+my $count2=0;
 my $collation='latin1_general_cs';
+my $update_time = "2011-11-08 12:26:41";
+
 foreach my $row (@$indexTable) {
+  
   $table="L".$row->{tableName}."L";
   print "$table\n";
-  $db->do("ALTER TABLE $table ADD (ownerId MEDIUMINT UNSIGNED  , gownerId MEDIUMINT UNSIGNED ), ADD FOREIGN KEY (ownerId) REFERENCES USERS(uId),ADD FOREIGN KEY (gownerId) REFERENCES GRPS(gId)" , {timeout=>[60000]} );
+  $update_time = $db->do("SELECT UPDATE_TIME FROM information_schema.tables WHERE table_schema='alice_users_sync' and table_name='$table'");
+  $update_status=$db->do("SELECT 1 FROM information_schema.tables WHERE table_schema='alice_users' AND table_name='$table' AND UPDATE_TIME>='$update_time'");
+  $count1=$db->do("SELECT count(*) FROM alice_users.$table");
+  $count2=$db->do("SELECT count(*) FROM alice_users_sync.$table");
+
+  ($update_status) or $update_status=0;
+  if($update_status==0 and $count1==$count2){
+    next;
+  }
+
+  $db->do("ALTER TABLE $table ADD (ownerId MEDIUMINT UNSIGNED  , gownerId MEDIUMINT UNSIGNED ), ADD FOREIGN KEY (ownerId) 
+   REFERENCES USERS(uId),ADD FOREIGN KEY (gownerId) REFERENCES GRPS(gId)" , {timeout=>[60000]} );
   $status=$db->do("select 1 from information_schema.tables where table_name='$table' and table_collation='$collation'");
   ($status) or $status=0;
   if($status==0){
@@ -58,11 +75,12 @@ foreach my $row (@$indexTable) {
     $db->do("ALTER TABLE $table COLLATE latin1_general_cs");
   }
 
-  $db->do("UPDATE $table JOIN USERS ON $table.owner=USERS.user JOIN GRPS ON $table.gowner=GRPS.user SET $table.ownerId=USERS.uId, $table.gownerId=GRPS.gId",{timeout=>[60000]} );
-  #  or $db->do("UPDATE $table JOIN USERS ON BINARY $table.owner=USERS.user SET $table.ownerId=USERS.uId") ;
-  # $db->do("UPDATE $table JOIN GRPS ON $table.gowner=GRPS.user SET $table.gownerId=GRPS.gId") 
-  #  or $db->do("UPDATE $table JOIN GRPS ON BINARY $table.gowner=GRPS.user SET $table.gownerId=GRPS.gId");
+  $db->do("UPDATE $table JOIN USERS ON $table.owner=USERS.user JOIN GRPS ON $table.gowner=GRPS.user 
+    SET $table.ownerId=USERS.uId, $table.gownerId=GRPS.gId",{timeout=>[60000]} );
   $db->do("ALTER TABLE $table DROP COLUMN owner, DROP COLUMN gowner",{timeout=>[60000]});
+  $db->do("DROP TABLE alice_users_sync.$table ",{timeout=>[60000]});
+  $db->do("ALTER TABLE alice_users.$table RENAME alice_users_sync.$table DROP COLUMN owner, DROP COLUMN gowner",{timeout=>[60000]});
+
 }
 print "New Changes made successfully !!!\n";
 print "\n".scalar(localtime(time))."\n";
@@ -76,10 +94,21 @@ foreach my $row (@$guidIndex) {
 }
 print "Doing the alteration in G#L tables\n";
 print "\n".scalar(localtime(time))."\n";
+
 foreach my $row (@$guidIndex) {
-  $table="G".$row->{tableName}."L";
+  $table="L".$row->{tableName}."L";
   print "$table\n";
-  $db->do("ALTER TABLE $table ADD (ownerId MEDIUMINT UNSIGNED  , gownerId MEDIUMINT UNSIGNED ), ADD FOREIGN KEY (ownerId) REFERENCES USERS(uId),ADD FOREIGN KEY (gownerId) REFERENCES GRPS(gId)" ,{timeout=>[60000]});
+  $update_time = $db->do("SELECT UPDATE_TIME FROM information_schema.tables WHERE table_schema='alice_users_sync' and table_name='$table'");
+  $update_status=$db->do("SELECT 1 FROM information_schema.tables WHERE table_schema='alice_users' AND table_name='$table' AND UPDATE_TIME>='$update_time'");
+  $count1=$db->do("SELECT count(*) FROM alice_users.$table");
+  $count2=$db->do("SELECT count(*) FROM alice_users_sync.$table");
+  ($update_status) or $update_status=0;
+  if($update_status==0 and $count1==$count2){
+    next;
+  }
+  
+  $db->do("ALTER TABLE $table ADD (ownerId MEDIUMINT UNSIGNED  , gownerId MEDIUMINT UNSIGNED ), ADD FOREIGN KEY (ownerId) 
+   REFERENCES USERS(uId),ADD FOREIGN KEY (gownerId) REFERENCES GRPS(gId)" , {timeout=>[60000]} );
   $status=$db->do("select 1 from information_schema.tables where table_name='$table' and table_collation='$collation'");
   ($status) or $status=0;
   if($status==0){
@@ -88,17 +117,15 @@ foreach my $row (@$guidIndex) {
     $db->do("ALTER TABLE $table COLLATE latin1_general_cs");
   }
 
-  $db->do("UPDATE $table JOIN USERS ON $table.owner=USERS.user JOIN GRPS ON $table.gowner=GRPS.user SET $table.ownerId=USERS.uId, $table.gownerId=GRPS.gId" ,{timeout=>[60000]}); 
-  # or $db->do("UPDATE $table JOIN USERS ON BINARY $table.owner=USERS.user SET $table.ownerId=USERS.uId") ;
-  # $db->do("UPDATE $table JOIN GRPS ON $table.gowner=GRPS.user SET $table.gownerId=GRPS.gId");
-  # or $db->do("UPDATE $table JOIN GRPS ON BINARY $table.gowner=GRPS.user SET $table.gownerId=GRPS.gId");
-  # $db->do("UPDATE $table join USERS ON $table.owner=USERS.user SET $table.ownerId= USERS.uId");
-  # $db->do("UPDATE $table join GRPS ON $table.gowner=GRPS.user SET $table.gownerId= GRPS.gId");
-  $db->do("ALTER TABLE $table DROP COLUMN owner, DROP COLUMN gowner" ,{timeout=>[60000]});
+  $db->do("UPDATE $table JOIN USERS ON $table.owner=USERS.user JOIN GRPS ON $table.gowner=GRPS.user 
+    SET $table.ownerId=USERS.uId, $table.gownerId=GRPS.gId",{timeout=>[60000]} );
+  $db->do("ALTER TABLE $table DROP COLUMN owner, DROP COLUMN gowner",{timeout=>[60000]});
+  $db->do("DROP TABLE alice_users_sync.$table ",{timeout=>[60000]});
+  $db->do("ALTER TABLE alice_users.$table RENAME alice_users_sync.$table DROP COLUMN owner, DROP COLUMN gowner",{timeout=>[60000]});
+
 }
 print "New Changes made successfully !!!\n";
 print "\n".scalar(localtime(time))."\n";
-
 
 print "Updating the GROUPS table to UGMAP with uId and gId & DROP columns Username and Groupname\n";
 $db->do("ALTER TABLE GROUPS DROP COLUMN Userid");
@@ -108,6 +135,6 @@ $db->do("UPDATE GROUPS join GRPS ON GRPS.user=GROUPS.Groupname SET GROUPS.Groupi
 $db->do("ALTER TABLE GROUPS RENAME UGMAP");
 $db->do("ALTER TABLE UGMAP DROP COLUMN Username, DROP COLUMN Groupname");
 
-
 print "\n".scalar(localtime(time))."\n";
+
 #=cut
