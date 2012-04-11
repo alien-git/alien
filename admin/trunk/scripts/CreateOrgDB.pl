f247978207ecf161a1344f106249999a1464ab35
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2012-04-11 08:04:28 +0000

    Removing the taskpriority database

--- admin/trunk/scripts/CreateOrgDB.pl
+++ admin/trunk/scripts/CreateOrgDB.pl
@@ -264,7 +264,7 @@ $now =~ s/\n//;
 print "Creating the tables in the database\n";
 $db->createCatalogueTables() or exit(-2);
 
-foreach my $dbtype ('TaskQueue', 'Transfer', 'IS', 'Admin', 'TaskPriority') {
+foreach my $dbtype ('TaskQueue', 'Transfer', 'IS', 'Admin', ) {
   print "Creating the $dbtype...";
   my $s="AliEn::Database::$dbtype";
 
@@ -276,7 +276,6 @@ foreach my $dbtype ('TaskQueue', 'Transfer', 'IS', 'Admin', 'TaskPriority') {
 		 #					DEBUG=>5,
 		}
 	       ) or exit(-2);
-  $dbtype =~ /TaskPriority/ and $d->checkPriorityValue('admin');
   print "Done with $?\n";
 }
 my @q=(
