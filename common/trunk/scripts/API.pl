30840a00d399c212d094d6a53cb9d1140570d611
psaiz <psaiz@4525493e-7705-40b1-a816-d608a930855b>
2012-07-26 15:09:08 +0000

    Removing another file

--- common/trunk/scripts/API.pl
+++
@@ -1,8 +0,0 @@
-use strict;
-use AliEn::API;
-my $username = (shift or $ENV{'USER'});
-my $api = new AliEn::API($username);
-$api or print STDERR "Cannot start API for user <$username>!\n" and die;
-$api->start();
-
-
