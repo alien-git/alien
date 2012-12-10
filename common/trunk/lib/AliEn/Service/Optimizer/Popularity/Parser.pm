4955893bf576099e7d2bf0fcbe53ea00db9accf1
nmanukya <nmanukya@4525493e-7705-40b1-a816-d608a930855b>
2012-12-10 21:06:33 +0000

    Implementing logtail command in Parser module.

--- common/trunk/lib/AliEn/Service/Optimizer/Popularity/Parser.pm
+++ common/trunk/lib/AliEn/Service/Optimizer/Popularity/Parser.pm
@@ -49,13 +49,15 @@ sub checkWakesUp {
       }
       if ($portion <= $currTime ) {
       	 if (-e $LogFile){
-      	    my $result = $self->FILLfileAccessInfoTable($startTime, $epochSTARTtime, $portion, $LogFile, $LogFileDate);
+      	    my $result = $self->FILLfileAccessInfoTable($startTime, $currTime, $epochSTARTtime, $portion, $LogFile, $LogFileDate);
       	    if ($result){
+      	
       	       $self->{DB}->do("update collectors set actions='0', startTime= DATE_ADD('$startTime',INTERVAL  $interval HOUR)  where name='$name'") and 
+	          
 	           $self->info("The $collectorName optimizer finished.") and return;
 	        }
 	        else {
-	           $self->setCollectorTaskPending($collectorName, $startTime) or return; 
+	           $self->setCollectorTaskPending($collectorName, $startTime) and return; 
 	        }
       	 }
       	 else {
@@ -65,7 +67,7 @@ sub checkWakesUp {
       	 }
       }
     }
-  	$self->info("The Parser optimizer finished");
+  	$self->info("The $collectorName optimizer finished");
  	return;
 }
 
@@ -75,11 +77,11 @@ sub FILLfileAccessInfoTable
 	 my $startTime=shift;
 	 
 	 $self->{DB}->do("UPDATE collectors SET actions='1' WHERE startTime='$startTime' AND name='$name'");
+	 my $currentTime=shift;
 	 my $epochSTARTtime = shift;
 	 my $portion = shift;
 	 my $LogFile = shift;
 	 my $LogFileDate = shift;
-	 my @time = localtime();
 	 my $success;
 	 my $accessTime;
 	 my $operation;
@@ -87,12 +89,16 @@ sub FILLfileAccessInfoTable
 	 my $seName;
 	 my $userName;
 	 my $epochaccessTIME;
-	 # This is the interval of time that Parser exrtacts info from Authen
-	 open(FILE, "$LogFile") or die("Unable to open file");
-	 my @data = <FILE>;
-     close(FILE);
-# READFILE:
-	 foreach my $line (@data) {
+	 
+     open my $pipe, "-|", "$ENV{ALIEN_ROOT}/bin/logtail", "$LogFile" or die "could not start logtail on $LogFile";
+
+	 my @data = <$pipe>;
+
+     
+ if (@data) {
+
+ # READFILE:
+ 	 foreach my $line (@data) {
 	    if ($line =~ m/authorize/){
 		 	if ( ($line !~ m/FAILED/) ){
 		   		($accessTime, $userName, $operation, $fileName, $seName) = ($line =~ m{^(\d+:\d+:\d+).+\'(\w+)\s\w+\s(\w+)\s(.+)\s(.+)\'$});
@@ -109,21 +115,44 @@ sub FILLfileAccessInfoTable
 		  		$self->{DB}->do("INSERT IGNORE INTO userInfo (userName) values ('$userName')") or return;
 		  		$self->{DB}->do("INSERT IGNORE INTO seInfo (seName) values ('$seName')") or return;
 		  	
-			  	# if (($epochSTARTtime <= $epochaccessTIME) && ($epochaccessTIME < $portion)) {
-		  		my $userId = $self->{DB}->queryRow("SELECT userId from userInfo where userName='$userName'")->{userId} ;
-		      	my $seId = $self->{DB}->queryRow("SELECT seId from seInfo where seName='$seName'")->{seId}; 
+
+   		  		  my $userId = $self->{DB}->queryRow("SELECT userId from userInfo where userName='$userName'")->{userId} ;
+		      	          my $seId = $self->{DB}->queryRow("SELECT seId from seInfo where seName='$seName'")->{seId}; 
 		               
-				my $result=$self->{DB}->do("INSERT IGNORE INTO fileAccessInfo (fileName, success, userId, accessTime, seId, operation) values ('$fileName', $success, $userId, '$accessTime', $seId, '$operation')");
-				$result or $self->info("Can not do inserting") and return;
-			  	#}
-			   	# next READFILE;
+				  my $result=$self->{DB}->do("INSERT IGNORE INTO fileAccessInfo (fileName, success, userId, accessTime, seId, operation) values ('$fileName', $success, $userId, '$accessTime', $seId, '$operation')");
+
+ 				  if ($result) {
+                                   $self->{DB}->do("insert ignore into collectors (name, actions, startTime) values ('HourlyCollector', '0' ,'$startTime')") 
+                                   or $self->info("Could not insert a task for HourlyCollector") and return;
+                                  }
+                                  else {
+                                   $self->info("Could not do insertions") and return;
+                                  }
+#                                  next READFILE;
+#                                }
 		  	}
 	    }
-	 }
-	
-	 $self->{DB}->do("insert ignore into collectors (name, actions, startTime) values ('HourlyCollector', '0' ,'$startTime')") or $self->info("Could not insert a task for HourlyCollector") and return;	
-	
-	 return 1;
-} 
 
+	 }
+  return 1;
+ } 
+ else {
+	$self->info("The file is empty");
+    if (($currentTime - $epochSTARTtime) >= 3600){
+     $self->info("There were not file accesses at this time period!");
+     $self->{DB}->do("update collectors set actions='0', startTime= DATE_ADD('$startTime',INTERVAL  $interval HOUR)  where name='$name'") 
+      	or $self->info("Could not update collectors table") and return;	
+	
+    }
+    my $acctime = $self->{DB}->queryColumn("select MIN(accessTime) from fileAccessInfo");
+	if ($acctime and $acctime->[0]) {
+    
+     my $result2 = $self->{DB}->do("INSERT IGNORE INTO collectors (name,actions, startTime) VALUES ('HourlyCollector', '0', '$acctime->[0]')");
+     $result2 or return;
+	}
+  return; 
+ }
+ 
+}
 1;
+
