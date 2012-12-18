9418dd0b7e82c7c569bfcafc3530fac228264773
agrigora <agrigora@4525493e-7705-40b1-a816-d608a930855b>
2012-12-18 17:18:29 +0000

    remove

--- common/trunk/scripts/date.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-my $date = time;
-
-print "Time " . localtime($date) . "\n  ";
-print "Sleeping for 60 seconds...\n";
-sleep(60);
-print "Time " . localtime($date) . "\n  ";
-print "Done!!!\n";
-
